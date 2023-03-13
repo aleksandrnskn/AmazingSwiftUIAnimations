5448175d9cd8a3aaedef3f65fc5cce8bbd1b6d54
Aleksandr NSKN <aleksandr.nskn@yandex.ru>
2023-03-13 19:12:37 +0300

    Initial Commit

---
+++ AmazingSwiftUIAnimations/AmazingSwiftUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmazingSwiftUIAnimationsApp.swift
+//  AmazingSwiftUIAnimations
+//
+//  Created by Aleksandr on 13.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct AmazingSwiftUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
